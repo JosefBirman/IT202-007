1c4ec34f5100522fb3550c3f076eee60e2e835cd
JosefBirman <70653512+JosefBirman@users.noreply.github.com>
2020-10-13 19:19:10 -0400

    Update home.php

--- project/home.php
+++ project/home.php
@@ -1,9 +1,10 @@
-<?php require_once(__DIR__ . "/partials/nav.php"?>
+
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 //we use this to safely get the email to display
 $email = "";
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- $email = $_SESSION["user"]["email"]; 
+if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
+    $email = $_SESSION["user"]["email"];
 }
 ?>
-<p>Welcome, <?php echo $email;?></p>
+<p>Welcome, <?php echo $email; ?></p>
