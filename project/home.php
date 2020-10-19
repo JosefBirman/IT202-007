99b82f50739ad144258431f5e3e359245c79fe45
JosefBirman <jrb77@njit.edu>
2020-10-19 02:57:08 -0400

    modified home.php

--- project/home.php
+++ project/home.php
@@ -1,14 +1,11 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
-//we use this to safely get the email to display
 $email = "";
 if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
     $email = $_SESSION["user"]["email"];
 }
 ?>
-<<<<<<< HEAD
     <p>Welcome, <?php echo $email; ?></p>
 <?php require(__DIR__ . "/partials/flash.php");
-=======
 <p>Welcome, <?php echo $email; ?></p>
->>>>>>> 1b37c37bf64b8231dc8abe108c025698c76e73a6
+
