09ca75981a0e7251c43031c5bdd25a6f8a60313d
JosefBirman <jrb77@njit.edu>
2020-12-17 23:16:00 -0500

    added a program that adds scores to PointsHistory, Scores, and Users tables

--- project/create_scores.php
+++ project/create_scores.php
@@ -2,13 +2,25 @@
 
 <?php
     if(is_logged_in() && isset($_POST["score"])){
-      $user_id = get_user_id();
       $score = $_POST["score"];
+      echo "Refresh the page to play again!";
       $db = getDB();
       $stmt = $db->prepare("INSERT INTO Scores (user_id, score) VALUES(:user_id, :score)");
       $stmt->execute([	
-      ":user_id"=>$user_id,
+      ":user_id"=>get_user_id(),
 		  ":score"=>$score
 	    ]);
+      
+      $stmt = $db->prepare("INSERT INTO PointsHistory (user_id, points_change, reason) VALUES(:user_id, :change, :reason)");
+      $stmt->execute([	
+      ":user_id"=>get_user_id(),
+		  ":change"=>$score/100,
+      ":reason"=>"Earned from game."
+	    ]);
+      
+      $sum = getBalance() + $_POST["score"]/100;
+      $stmt = $db->prepare("UPDATE Users set points=:points where id= :id");
+      $r = $stmt->execute([":points"=>$sum, ":id"=>get_user_id()]);
     }
+    
 ?>
